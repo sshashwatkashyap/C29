fb6a4ab8b212cfa7ead8fc6cdbe6a7deb6ec0598
sshashwatkashyap <ssuchitrasingh@gmail.com>
2020-10-12 20:51:43 +0530

    SlingShot Class

---
+++ SlingShot.js
@@ -0,0 +1,34 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.slingShot = Constraint.create(options);
+        this.pointB = pointB
+	World.add(world, this.slingShot);
+    }
+
+    display(){
+
+        if(this.slingShot.bodyA){
+
+        var pointA = this.slingShot.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+	    
+	}	
+    }
+
+   fly(){
+
+    this.slingShot.bodyA = null 
+
+   }	   
+
+}
+
+
